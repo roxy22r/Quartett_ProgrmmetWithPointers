b6f119509b3a017df7cc020477a1984131055bbb
raksana ravichandran <raksana.ravichandran@loewenfels.ch>
2021-11-27 16:03:12 +0100

    M411 Added main input

--- Quartett/Quartett.cpp
+++ Quartett/Quartett.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+
+typedef struct Card{
+    char Bez[50];
+    int usetime ; 
+    double wight;
+    struct Card* pNext;
+} struCard;
+
+bool checkInputValue(int *pvalueToCompare) {
+    if (*pvalueToCompare==1||*pvalueToCompare==2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int inputCompearValue() {
+    int valueToCompare;
+    int* pvalueToCompare = &valueToCompare; 
+    printf("Geben Sie den Vergleichs wert ein: \n");
+    scanf_s("%i", pvalueToCompare);
+    printf("%i", valueToCompare);
+    if (checkInputValue(pvalueToCompare)) {
+        return *pvalueToCompare;
+    }
+ 
+    inputCompearValue();
+    
+}
+
+  
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    inputCompearValue();
+    
 }
+
 
