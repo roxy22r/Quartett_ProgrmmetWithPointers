059456b840b6254993b7ad2c43a60a84ccc3fe0f
vmadmin <vmadmin@BMWP1>
2021-12-03 09:04:46 +0100

    M411 add needed methods with description

--- Quartett/Quartett.cpp
+++ Quartett/Quartett.cpp
@@ -12,35 +12,73 @@ typedef struct Card{
 } struCard;
 
 bool checkInputValue(int *pvalueToCompare) {
-    if (*pvalueToCompare==1||*pvalueToCompare==2)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return  (*pvalueToCompare == 1 || *pvalueToCompare == 2);
+    
+   
 }
-int inputCompearValue() {
+
+void welcome() {
+//name input player 
+//and short explaination of game
+}
+ 
+void addCardsTofakeList () {
+  
+    //add Carts to spesific address 
+}
+void radomMixOfCardStack() {
+    //self explaint
+}
+void distributeCardsToPlayers() {
+    //Give each player their Stack of Cards
+}
+void getFirstElementOfCardStack() {
+    //Get First element of player and enemy of Stack ^^
+}
+int inputCompareValue() {
     int valueToCompare;
-    int* pvalueToCompare = &valueToCompare; 
+    int* pvalueToCompare = &valueToCompare;
     printf("Geben Sie den Vergleichs wert ein: \n");
     scanf_s("%i", pvalueToCompare);
     printf("%i", valueToCompare);
     if (checkInputValue(pvalueToCompare)) {
         return *pvalueToCompare;
     }
- 
-    inputCompearValue();
-    
+
+    inputCompareValue();
+
 }
 
-  
+char defineHigherCard(Card* playerCard, Card* enemyCard, int* valueToCheck) {
+    char player = 'P';
+    char enemy = 'E';
+    char* pPlayer = &player;
+    char* pEnemy = &player;
+
+    if ((*valueToCheck == 1 && playerCard->usetime > enemyCard->usetime) || (*valueToCheck == 2 && playerCard->wight > enemyCard->wight))
+    {
+        return *pPlayer;
+    }
+    else {
+        return *pEnemy;
+    }
+}
+
+void addCardToListOfWinner() {
+    //Get first cart of loser and set behinde stack of winner with first card of winner
+}
+void checkListSizeOfPlayers() {
+    //Check the List Of Players every round
+    //if 0 ||/0 than winner is Player with not Zero Cards
+}
+void showWinner() {
+    //show who and ask if player wannt's to play again
+}
 
 int main()
 {
-    inputCompearValue();
-    
+    inputCompareValue();
+    //add methods togehter
 }
 
 
