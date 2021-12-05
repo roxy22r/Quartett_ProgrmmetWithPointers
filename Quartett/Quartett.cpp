f27e0d38d88ee601f5c1c4b9bb63ad81c2f69287
roxy22r <raksana280@gmail.com>
2021-12-05 03:13:29 +0100

    M411 created methode remove, moveUp, add and one Unfinished method with bugss

--- Quartett/Quartett.cpp
+++ Quartett/Quartett.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <string>
 
+
 typedef struct Card{
     char Bez[50];
     int usetime ; 
@@ -11,41 +12,117 @@ typedef struct Card{
     struct Card* pNext;
 } struCard;
 
-bool checkInputValue(int *pvalueToCompare) {
-    return  (*pvalueToCompare == 1 || *pvalueToCompare == 2);
-    
-   
+typedef struct Player {
+    char name[50];
+    Card* pfirstCardOfList;
+    Card* pLastCardOfList;
+}struPlayer;
+
+
+// author of mehtod Raksana
+void Output(Card* firstCard) {
+    Card* pTemp = firstCard;
+    while (pTemp != NULL)
+    {
+        printf(" %s\n", pTemp->Bez);
+        printf(" usetime: %i\n", pTemp->usetime);
+        printf(" wight: %lf\n", pTemp->wight);
+        printf("\n");
+        pTemp = pTemp->pNext;
+    }
+
+
+
 }
-//TODO: Raksana
-void removeCard() {
+/*
+* Author of mehtod Raksana
+* Dise Methode schaut,ob der Wert 1 oder 2 ist
+*/
+bool checkInputValue(int *pvalueToCompare) {
+    
+        return  (*pvalueToCompare == 1 || *pvalueToCompare == 2);
+    
 }
 
-//TODO: Raksana
-void addCard() {
+
+/*
+* Author of mehtod Raksana
+* Dise Methode schiebt die Karte eins nach Oben
+*/
+Card *moveCardUp(Player* pPlayer,Card* moveTo) {
+    Card* pTemp = pPlayer->pfirstCardOfList;
+    while (true)
+    {
+        if (pTemp->pNext==moveTo) {
+            pTemp->pNext = moveTo->pNext;
+            moveTo->pNext = NULL;
+            return moveTo;
+        }                                  
+        pTemp = pTemp->pNext;
+    }
+   
+
 }
+
+/*
+* Author of mehtod Raksana
+* Dise Methode fügt die Karte zum schluss hinzu
+*/
+void addCard(Card *cardToAdd,Player* pPlayer) {
+    cardToAdd->pNext = NULL;
+    pPlayer->pLastCardOfList->pNext= cardToAdd;
+    pPlayer->pLastCardOfList = pPlayer->pLastCardOfList->pNext;
+    
+    printf("============================");
+    Output(pPlayer->pfirstCardOfList);
+}
+/*
+* Author of mehtod Raksana
+* Diese Methode entfernt die Karte schiebt, die näachte Karte hoch 
+* und fügt die Karte zum Schluss von dem Talon hinzu
+*/
+
+void removeCard(Player* pPlayer, Card *pCardToRemove) {
+    Card* pTemp= pPlayer->pfirstCardOfList;
+    while (pTemp->pNext!= pCardToRemove->pNext->pNext)
+    {
+        if (pTemp==pCardToRemove)
+        {
+            Card* removedCard = pPlayer->pfirstCardOfList;
+            Card* temp=moveCardUp(pPlayer,pPlayer->pfirstCardOfList);
+            addCard(removedCard, pPlayer);
+            Output(pPlayer->pfirstCardOfList);
+        }
+        pTemp=pTemp->pNext;
+    }
+}
+
+
 //TODO: Raksana
 void welcome() {
 //name input player 
 //and short explaination of game
 }
 
- //TODO: Tamara
-void addCardsTofakeList () {
-  
-    //add Carts to spesific address 
-}
+                       
 //TODO: Raksana
-void radomMixOfCardStack(Card *pFirstCard,Card *pLastCard) {
-    //self explaint
+void radomMixOfCardStack(Player *pPlayer) {
+    
+    moveCardUp(pPlayer,pPlayer->pLastCardOfList);
+    printf("/////////////////////////////////////////////////");
+    Output(pPlayer->pfirstCardOfList);
+    
+
 }
 //TODO: Tamara
-void distributeCardsToPlayers(Card* pFirstCard, Card* pLastCard) {
+void distributeCardsToPlayers(Player player,Player enemy) {
     //Give each player their Stack of Cards
 }
 //TODO: Tamara
 void getFirstElementOfCardStack() {
     //Get First element of player and enemy of Stack ^^
 }
+// author of mehtod Raksana
 int inputCompareValue() {
     int valueToCompare;
     int* pvalueToCompare = &valueToCompare;
@@ -60,6 +137,7 @@ int inputCompareValue() {
 
 }
 
+// author of mehtod Raksana
 char defineHigherCard(Card* playerCard, Card* enemyCard, int* valueToCheck) {
     char player = 'P';
     char enemy = 'E';
@@ -88,10 +166,70 @@ void showWinner() {
     //show who and ask if player wannt's to play again
 }
 
+
+
+
+
+
 int main()
 {
-    inputCompareValue();
-    //add methods togehter
-}
+   
+    struCard* pcard  = (struCard*)malloc(sizeof(struCard));
+    strcpy_s(pcard->Bez,"1Card");
+    pcard->usetime = 2;
+    pcard->wight = 1.3;
+    pcard->pNext = NULL;
+    struCard* pStartPlayer=pcard;
+    //
+    pcard = (struCard*)malloc( sizeof(struCard));
+    strcpy_s(pcard->Bez, "2Card");
+    pcard->usetime = 3;
+    pcard->wight = 1.5;
+    pcard->pNext = NULL;
+    pStartPlayer->pNext = pcard;
+    struCard* pCard2 = pcard;
+    //
+    pcard = (struCard*)malloc(sizeof(struCard));
+    strcpy_s(pcard->Bez, "3Card");
+    pcard->usetime = 4;
+    pcard->wight = 1.7;
+    pcard->pNext = NULL;
+    pCard2->pNext = pcard;
+    struCard* pCard3 = pcard;
+    //
+    pcard = (struCard*)malloc(sizeof(struCard));
+    strcpy_s(pcard->Bez, "4Card");
+    pcard->usetime = 5;
+    pcard->wight = 8.6;
+    pcard->pNext = NULL;
+    pCard3->pNext = pcard;
+    struCard* pCard4 = pcard;
+    //
+    pcard = (struCard*)malloc(sizeof(struCard));
+    strcpy_s(pcard->Bez, "5Card");
+    pcard->usetime = 6;
+    pcard->wight = 4.6;
+    pcard->pNext = NULL;
+    pCard4->pNext = pcard;
+    struCard* pCard5 = pcard;
+    //
+    //    
+    pcard = (struCard*)malloc(sizeof(struCard));
+    strcpy_s(pcard->Bez, "6Card");
+    pcard->usetime = 7;
+    pcard->wight = 9.6;
+    pcard->pNext = NULL;
+    pCard5->pNext = pcard;
+    struCard* pCard6 = pcard;
+    //strat eneamy Card Talon
 
+    struPlayer* pPlayer = (Player*)malloc(sizeof(Player));
+    pPlayer->pfirstCardOfList = pStartPlayer;
+    pPlayer->pLastCardOfList = pCard6;
+    radomMixOfCardStack(pPlayer);
+    printf("@@@@@@@@@@@@@@@@@@@@");
+    Output(pStartPlayer);
+    system("pause");
+    return 0;
+}
 
